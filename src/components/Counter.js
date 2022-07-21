2d6d15e5c40dab597fae16515fd28e26954c68ad
cailu <cailudys@gmail.com>
2022-07-21 14:20:29 +0800

    使用redux-react链接react和redux

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function Counter(props) {
+  return (
+    <div>
+      <button onClick={props.increment}>+</button>
+      <span>{props.count}</span>
+      <button onClick={props.decrement}>-</button>
+    </div>
+  );
+}
+
+// 此函数要求返回值是一个对象，对象里的属性都会被映射到组件的props对象上
+// 也就是说如果返回值是{a:1}  那么在Counter组件中就可以通过props.a取到1
+const mapStateToProps = (state) => ({
+  count: state.count,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  increment() {
+    dispatch({ type: "increment" });
+  },
+  decrement() {
+    dispatch({ type: "decrement" });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
